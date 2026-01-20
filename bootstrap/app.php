fb2311e778390d06a50269d59e3f8430bc589db6
Nabil <issakanabiltoure5@gmail.com>
2026-01-20 19:56:13 +0000

    Fix CORS configuration for Laravel 10

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,24 +1,40 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Middleware;
+/*
+|--------------------------------------------------------------------------
+| Create The Application
+|--------------------------------------------------------------------------
+*/
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/routes/api.php',
-        commands: __DIR__.'/routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Application $app) {
-        $app->middleware([
-            \Illuminate\Http\Middleware\HandleCors::class,
-        ]);
+$app = new Illuminate\Foundation\Application(
+    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
+);
 
-        $app->middlewareGroup('api', [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ]);
-    })
-    ->create();
+/*
+|--------------------------------------------------------------------------
+| Bind Important Interfaces
+|--------------------------------------------------------------------------
+*/
+
+$app->singleton(
+    Illuminate\Contracts\Http\Kernel::class,
+    App\Http\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Console\Kernel::class,
+    App\Console\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
+    App\Exceptions\Handler::class
+);
+
+/*
+|--------------------------------------------------------------------------
+| Return The Application
+|--------------------------------------------------------------------------
+*/
+
+return $app;
